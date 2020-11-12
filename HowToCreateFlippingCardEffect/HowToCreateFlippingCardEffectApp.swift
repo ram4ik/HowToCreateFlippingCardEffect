651199b73c99f00b01e6ee5189d1e5267d689849
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-11-12 14:54:34 +0200

    Initial Commit

---
+++ HowToCreateFlippingCardEffect/HowToCreateFlippingCardEffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  HowToCreateFlippingCardEffectApp.swift
+//  HowToCreateFlippingCardEffect
+//
+//  Created by ramil on 12.11.2020.
+//
+
+import SwiftUI
+
+@main
+struct HowToCreateFlippingCardEffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
